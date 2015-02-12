5d4ce4fd78a0b43c38fd6cc57cca021132b5a2b8
Jamie Hutber - Home <jamie@hutber.com>
2015-02-12 08:54:49 +0000

    fixed susy compile error

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,37 +1,38 @@
-//var gulp = require('gulp');
-//var handleErrors = require('../util/handleErrors');
-//var sourcemaps = require('gulp-sourcemaps');
-//var minifyCSS = require('gulp-minify-css');
-//
-////libsass
-//var sass = require('gulp-sass');
-//gulp.task('sass', function () {
-//	return gulp.src('./frontend/sass/style.scss')
-//		.pipe(sourcemaps.init())
-//		.pipe(sass({
-//			'require':'susy',
-//			loadPath: [
-//				'C:/Ruby193/lib/ruby/gems/1.9.1/gems/susy-2.2.2/sass'
-//			]
-//		}))
-//		//.pipe(minifyCSS())
-//		.pipe(sourcemaps.write('./app/www/css'))
-//		.on('error', handleErrors)
-//		.pipe(gulp.dest('./app/www/css'))
-//});
-
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var handleErrors = require('../util/handleErrors');
+var sourcemaps = require('gulp-sourcemaps');
 var minifyCSS = require('gulp-minify-css');
+var debug = require('gulp-debug');
 
+//libsass
 gulp.task('sass', function () {
 	return gulp.src('./frontend/sass/style.scss')
+		.pipe(sourcemaps.init())
+		//.pipe(debug({title: 'unicorn:'}))
 		.pipe(sass({
-			'sourcemapPath':'./app/www/css',
-			'require':'susy'
+			includePaths: [
+				'./node_modules/susy/sass'
+			]
 		}))
 		//.pipe(minifyCSS())
+		.pipe(sourcemaps.write('./app/www/css'))
 		.on('error', handleErrors)
 		.pipe(gulp.dest('./app/www/css'))
 });
+
+//var gulp = require('gulp');
+//var sass = require('gulp-ruby-sass');
+//var handleErrors = require('../util/handleErrors');
+//var minifyCSS = require('gulp-minify-css');
+//
+//gulp.task('sass', function () {
+//	return gulp.src('./frontend/sass/style.scss')
+//		.pipe(sass({
+//			'sourcemapPath':'./app/www/css',
+//			'require':'susy'
+//		}))
+//		//.pipe(minifyCSS())
+//		.on('error', handleErrors)
+//		.pipe(gulp.dest('./app/www/css'))
+//});
