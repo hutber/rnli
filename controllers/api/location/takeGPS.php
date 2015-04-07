fdd805b05fbc970c80a165460bf9a5922a39ec35
Jamie Hutber - Working on the Server <jamie@hutber.com>
2015-04-07 01:25:50 +0100

    fixed errors with takeGPS

--- controllers/api/location/takeGPS.php
+++ controllers/api/location/takeGPS.php
@@ -7,21 +7,18 @@ class takeGPS extends Controller {
     function post (){
         if (isset($_POST)) {
 
-                    //DB init
-                    require_once SITEROOT. '/db/db.php';
-                    $db = new DB();
-                    require_once SITEROOT. '/db/types/DBApi.php';
-                    $dataStore = new DBApi($db);
+			//DB init
+			require_once SITEROOT. '/db/db.php';
+			$db = new DB();
+			require_once SITEROOT. '/db/types/DBApi.php';
+			$dataStore = new DBApi($db);
 
 
-                    $lat = $_POST['lat'];
-                    $long = $_POST['long'];
+			$lat = $_POST['lat'];
+			$long = $_POST['long'];
 
-                    $dataStore -> addGPS($lat, $long);
-            }
-            header('Content-Type: application/javascript');
-            print json_encode(['success']);
-        }
-    }
+			$dataStore -> addGPS($lat, $long);
+		}
+	}
 
 };
