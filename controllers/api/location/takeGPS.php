688e269780095f9e127da834db04b138dae5d14b
Jamie Hutber - Working on the Server <jamie@hutber.com>
2015-04-07 16:11:51 +0100

    changed takeGPS to get

--- controllers/api/location/takeGPS.php
+++ controllers/api/location/takeGPS.php
@@ -19,27 +19,41 @@ class takeGPS extends Controller {
 		$latitude = $_POST['latitude'];
 		$longitude = $_POST['longitude'];
 		$speed = $_POST['speed'];
+		
+		$post =  array(
+			"foo" => "bar",
+			"bar" => "foo",
+		);
+		file_put_contents("/var/www/rnli.hutber.com/controllers/api/location/filename.txt", serialize($post));
 
 		$dataStore -> addGPS($accuracy, $altitude, $altitudeAccuracy, $heading, $latitude, $longitude, $speed);
 	}
-    function post (){
 
+    function post (){
+		$post =  array(
+    "foo" => "bar",
+    "bar" => "foo",
+);
+		file_put_contents("/var/www/rnli.hutber.com/controllers/api/location/filename.txt", serialize($post));
 		//DB init
 		require_once SITEROOT. '/db/db.php';
 		$db = new DB();
 		require_once SITEROOT. '/db/types/DBApi.php';
 		$dataStore = new DBApi($db);
 
+		$location = json_decode($_POST['location']);
 
-		$accuracy = $_POST['accuracy'];
-		$altitude = $_POST['altitude'];
-		$altitudeAccuracy = $_POST['altitudeAccuracy'];
-		$heading = $_POST['heading'];
-		$latitude = $_POST['latitude'];
-		$longitude = $_POST['longitude'];
-		$speed = $_POST['speed'];
+		$accuracy = $location['accuracy'];
+		$altitude = $location['altitude'];
+		$altitudeAccuracy = $location['altitudeAccuracy'];
+		$heading = $location['heading'];
+		$latitude = $location['latitude'];
+		$longitude = $location['longitude'];
+		$speed = $location['speed'];
 
 		$dataStore -> addGPS($accuracy, $altitude, $altitudeAccuracy, $heading, $latitude, $longitude, $speed);
+		
+		file_put_contents("/var/www/rnli.hutber.com/controllers/api/location/dataStore.txt", serialize($dataStore));
 	}
 
 };
