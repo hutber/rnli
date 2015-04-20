8e79dc8dc208302e18a34b7352058c09dbed08a0
Jamie Hutber - Working on the Server <jamie@hutber.com>
2015-04-20 21:54:32 +0100

    uploading images

--- controllers/app/users/uploadProfileImage.php
+++ controllers/app/users/uploadProfileImage.php
@@ -3,14 +3,21 @@
 class uploadProfileImage extends Controller {
     function post()
     {
-		$uploaddir = '/var/www/uploads/';
-		$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-		r($_FILES);
-		if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-			echo "File is valid, and was successfully uploaded.\n";
-		} else {
-			echo "Upload failed";
-		}
+		if ($_REQUEST['image']) {
 
+            // convert the image data from base64
+            $imgData = base64_decode($_REQUEST['image']);
+
+            // set the image paths
+            $file = '/var/www/rnli.hutber.com/uploads/profile/profileImage.jpg';
+
+            // delete the image if it already exists
+            if (file_exists($file)) { unlink($file); }
+
+            // write the imgData to the file
+            $fp = fopen($file, 'w');
+            fwrite($fp, $imgData);
+            fclose($fp);
+		}
     }
 }
